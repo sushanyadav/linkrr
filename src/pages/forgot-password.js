92e20063b364b7602cebfd2af45f15cfa833c230
Sushan Yadav <sushanyadav98@gmail.com>
2021-07-22 11:57:26 +0545

    restricted forgot page when logged in

--- src/pages/forgot-password.js
+++ src/pages/forgot-password.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { getSession } from "next-auth/client";
 
 import { validateEmail } from "utils/validate";
 
@@ -81,5 +82,19 @@ const ForgotPasswordPage = () => {
     </div>
   );
 };
+
+export async function getServerSideProps(context) {
+  const session = await getSession({ req: context.req });
+
+  if (session) {
+    return {
+      redirect: { destination: "/", permanent: false },
+    };
+  }
+
+  return {
+    props: { session },
+  };
+}
 
 export default ForgotPasswordPage;
